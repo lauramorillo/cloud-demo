29acb12746858f570a818922989f60cc8c91cf75
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2022-03-02 20:31:18 +0100

    Adding app example

---
+++ app/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/img/pizza.png')
+})
+
+app.listen(port, () => {
+  console.log(`Pizza app listening on port ${port}`)
+})
