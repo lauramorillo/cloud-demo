32b3004bfde3aae338998f0991e420fce729da5b
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2022-03-03 23:14:15 +0100

    Deleting also repository for GKE test

--- gke/delete.sh
+++ gke/delete.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
+PROJECT_ID=[YOUR_PROJECT]
 
 gcloud container clusters delete demo-cluster \
     --zone us-central1-f \
     --quiet
+
+# Clean repository
+image=gcr.io/${PROJECT_ID}/demo-repo
+for digest in $(gcloud container images list-tags $image --limit=unlimited --format='get(digest)'); do
+(
+    set -x
+    gcloud container images delete -q --force-delete-tags "${image}@${digest}"
+)
+done
